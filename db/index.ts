8b374339a0c82d2e5fe8e5eceaabc1b5b3f30ff0
ranisans <ranisans@yahoo.com>
2021-01-18 13:21:33 +0300

    fix: restore generated PrismaClient by default in blitz

--- db/index.ts
+++ db/index.ts
@@ -2,15 +2,16 @@ import { PrismaClient } from "@prisma/client";
 
 export * from "@prisma/client";
 
-const getPrisma = (): PrismaClient => {
-  if (process.env.NODE_ENV === "production") {
-    return new PrismaClient();
-  }
+// eslint-disable-next-line import/no-mutable-exports
+let prisma: PrismaClient;
+
+if (process.env.NODE_ENV === "production") {
+  prisma = new PrismaClient();
+} else {
   // Ensure the prisma instance is re-used during hot-reloading
   // Otherwise, a new client will be created on every reload
-  return globalThis.prisma || new PrismaClient();
-};
-
-const prisma = getPrisma();
+  globalThis.prisma = globalThis.prisma || new PrismaClient();
+  prisma = globalThis.prisma;
+}
 
 export default prisma;
