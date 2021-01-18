eb96a15da9c93f14e4136aae79be622832665416
ranisans <ranisans@yahoo.com>
2021-01-18 12:54:53 +0300

    feat: add eslint, editorconfig,  gitignore, tsconfig rules feat: add package to eslint rules fix: add used packages in dependency by ESLint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,54 @@
 module.exports = {
+  parser: "@typescript-eslint/parser",
+  extends: [
+    "airbnb",
+    "airbnb/hooks",
+    "react-app",
+    "prettier/react",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:jest/recommended",
+    "plugin:jest/style",
+    "plugin:jest-dom/recommended",
+    "plugin:testing-library/react",
+    "plugin:prettier/recommended",
+    "prettier/@typescript-eslint",
+  ],
+  plugins: ["@typescript-eslint", "jest", "jest-dom", "testing-library", "prettier"],
+  settings: {
+    "import/resolver": {
+      typescript: {},
+    },
+  },
   env: {
-    es2020: true,
+    browser: true,
+    node: true,
+    es6: true,
+    jest: true,
   },
-  extends: ['react-app', 'plugin:jsx-a11y/recommended'],
-  plugins: ['jsx-a11y'],
   rules: {
-    "import/no-anonymous-default-export": "error",
-    'import/no-webpack-loader-syntax': 'off',
-    'react/react-in-jsx-scope': 'off', // React is always in scope with Blitz
-    'jsx-a11y/anchor-is-valid': 'off', //Doesn't play well with Blitz/Next <Link> usage
+    "global-require": "off",
+    "no-console": "off",
+    "no-underscore-dangle": "off",
+    "no-param-reassign": "off",
+    "react-hooks/exhaustive-deps": "off",
+    "react/jsx-props-no-spreading": "off",
+    "react/jsx-filename-extension": [
+      "error",
+      {
+        extensions: [".js", ".jsx", ".ts", ".tsx"],
+      },
+    ],
+    "import/extensions": "off",
+    "import/no-extraneous-dependencies": [
+      "error",
+      {
+        devDependencies: true,
+      },
+    ],
+    "@typescript-eslint/ban-ts-comment": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-var-requires": "off",
+    "prettier/prettier": "error",
+    "react/require-default-props": "off",
   },
-}
+};
