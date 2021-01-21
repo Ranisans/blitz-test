ecf9a960d9e4feede0a4c9a3150064397cc55436
ranisans <ranisans@yahoo.com>
2021-01-21 15:02:36 +0300

    feat: add first row for objects page add clsx dependency

---
+++ app/objects/components/FirstRow.tsx
@@ -0,0 +1,69 @@
+import React from "react";
+import { FormControl, InputLabel, Select, MenuItem, Button } from "@material-ui/core";
+import { makeStyles, Theme } from "@material-ui/core/styles";
+import clsx from "clsx";
+import excelIcon from "public/excel-icon.png";
+import mapIcon from "public/map-icon.png";
+import { SORTING_VALUES } from "../constants";
+
+const useStyles = makeStyles((theme: Theme) => ({
+  container: {
+    display: "flex",
+    flexDirection: "row-reverse",
+  },
+  flexContainer: {
+    display: "flex",
+    flexWrap: "wrap",
+    alignItems: "end",
+    "& > *": {
+      marginRight: 5,
+    },
+  },
+  imageButton: {
+    padding: theme.spacing(0, 0),
+    backgroundColor: "#ccc",
+    backgroundRepeat: "no-repeat",
+    backgroundPosition: "center",
+    boxSizing: "border-box",
+    maxWidth: 50,
+    maxHeight: 50,
+    minWidth: 50,
+    minHeight: 50,
+  },
+  excelButton: {
+    backgroundImage: `url(${excelIcon})`,
+  },
+  mapButton: {
+    backgroundImage: `url(${mapIcon})`,
+  },
+}));
+
+const FirstRow: React.FC = () => {
+  const styles = useStyles();
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.flexContainer}>
+        <FormControl style={{ width: 125 }}>
+          <InputLabel htmlFor="objects-sorting_type">Сортировка по</InputLabel>
+          <Select
+            inputProps={{
+              name: "sortingType",
+              id: "object_list-sorting_type",
+            }}
+          >
+            {SORTING_VALUES.map((item) => (
+              <MenuItem key={item.label} value={item.value}>
+                {item.label}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+        <Button aria-label="to excel" className={clsx(styles.imageButton, styles.excelButton)} />
+        <Button aria-label="to map" className={clsx(styles.imageButton, styles.mapButton)} />
+      </div>
+    </div>
+  );
+};
+
+export default FirstRow;
---
+++ app/objects/constants/index.ts
@@ -0,0 +1,12 @@
+export const SORTING_VALUES = [
+  { label: "Нач. Охраны", value: 1 },
+  { label: "Заместители", value: 2 },
+  { label: "ЧОПы", value: 3 },
+  { label: "Маршруты", value: 4 },
+];
+
+export const CHECK_TYPES = [
+  { label: "Все", value: 0 },
+  { label: "Обычная", value: 1 },
+  { label: "По указанию", value: 2 },
+];
